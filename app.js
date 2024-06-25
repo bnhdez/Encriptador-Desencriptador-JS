262b857d8f13e470ffd797996f6f301892b8bf7a
borishdez <borisnandez93@gmail.com>
2024-06-24 21:49:45 -0600

    metodos guardadoDatos & mostrarDisplay

---
+++ app.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const textarea = document.getElementById("datos");
+    const saveButton = document.getElementById("encriptar");
+    let datos = [];
+    let display = true;
+
+    
+    saveButton.addEventListener("click", () => {
+        
+        guardadoDatos();
+    })
+    function guardadoDatos(){
+        const newtext = textarea.value;
+        //verifico que vengan datos
+        if (!newtext) {
+            alert("Por favor ingresa tu texto a encriptar");
+            textarea.value = '';
+        } else {
+        //agrego el texto al array
+            datos.push(newtext);
+            console.log(datos);
+            textarea.value = ''; // limpieza texto
+            display = false;
+        }
+        console.log(display)
+        mostrarDisplay()
+    }
+    function mostrarDisplay() {
+        if (display === true) {
+            return;
+        } else {
+            document.getElementById("muñeco").style.display = "none";
+            document.getElementById("titulo-display").style.display = "none";
+            document.getElementById("texto-display").style.display = "none";
+        }
+    }
+    ;
+});
